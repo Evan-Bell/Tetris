95f1757920e4c714bf6ebaddb88e3097126bbdf0
Evan-Bell <evanbell888@yahoo.com>
2022-05-31 15:55:16 -0400

    App.js changed to use hooks, be more efficient, allow controls on whole page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 import StylizedBoardDisplay from "./BoardDisplay"
 import ControlsDisplay from './Controls';
 import Board from "./game"
+import useKeyPress from './useKeyPress';
+
 
 
 
@@ -10,54 +13,21 @@ const height = 24; //playable height is 20
 
 
 var gameBoard = new Board(width, height);
-  
- //ADD TIME DROPS AND TIME DELAY
- //IMPROVE THE CSS/REACT/JS ASPECT OF SHOWING SO IT IS LESS LAGGY AND LOOKS BETTER
- // MAKE IT SO THE KEYPRESS IS APPLIED WHENEVER SOMEONE IS IN THE TETRIS WINDOW / MOUSE PRESSED IT
- //LEARN CSS FOR GOD SAKE
 
 
- export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            game_board: gameBoard,
-            score: {},
-            level:{},
-            hold: {},
-            next:{},
-            highscore:{},
-            highlevel: {},
-            droptime: {},
-            interval: {}
-        };
-    }
+function App() {
 
-    componentDidMount() {
+    const [game_board, update_board] = useState[gameBoard.board]
+
+    useEffect(() => {
         this.timerID = setInterval(
-          () => this.update_board(),
-          100
-        );
-      }
-    
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-    
-    update_board() {
-        this.setState({
-            game_board: gameBoard
-        });
-    }
+            () => update_board(gameBoard.board),
+            100
+          );
+      }, gameBoard.board)
 
-    handleKeyPress = (event) => {
-        if(event.key === 'Enter'){
-          console.log('enter press here! ')
-        }
-    }
-
-    handleInput(event){ //MAKE THIS AUTOCLICK INSTEAD OF HAVING TO GO INTO TEXT BOX
-        switch(event.key) {
+    useKeyPress(key => {
+        switch(key) {
             case 'ArrowLeft': 
                 gameBoard.move_left();
               break;
@@ -82,19 +52,15 @@ var gameBoard = new Board(width, height);
             case ' ':
                 gameBoard.hard_drop();
                 break;
-        }
-    }
-    
-    
-    render() {
-        return (
+        };
+    });
+
+    return(
             <div className="TetrisGame">
-                <h2>{`Interval: ${this.interval}`}</h2>
-                <input type='text' onKeyDown={this.handleInput} onChange={e => this.update_board()}/>
-                <input type="range" min="1" value={this.interval} max="100" onChange={e => this.update_board()}/>
-                <ControlsDisplay inputboard={this.state.game_board}/>
-                <StylizedBoardDisplay inputboard={this.state.game_board} inputheight={height} inputwidth={width}/>
+                <ControlsDisplay inputboard={game_board}/>
+                <StylizedBoardDisplay inputboard={game_board} inputheight={height} inputwidth={width}/>
             </div>
-        );
-     }
+    )
 }
+
+export default App;
