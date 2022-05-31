8d060a53f469c396bdb164e10e9b61e952abf28f
Evan-Bell <evanbell888@yahoo.com>
2022-05-31 15:22:35 -0400

    Updates

---
+++ precache-manifest.df3ef68606cd95fcd80fee9ef04ed105.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "218f7b76469528b7bc9e",
+    "url": "/Tetris/static/css/main.6944044c.chunk.css"
+  },
+  {
+    "revision": "218f7b76469528b7bc9e",
+    "url": "/Tetris/static/js/main.ddd84b06.chunk.js"
+  },
+  {
+    "revision": "c169349216605256f316",
+    "url": "/Tetris/static/js/runtime~main.cfe7b2d8.js"
+  },
+  {
+    "revision": "2393244554600308a11a",
+    "url": "/Tetris/static/js/2.34734fb2.chunk.js"
+  },
+  {
+    "revision": "f76f15b609472adf531e",
+    "url": "/Tetris/static/js/3.bfc2c5aa.chunk.js"
+  },
+  {
+    "revision": "b1a180c65b3049ca85432d3b50fc3600",
+    "url": "/Tetris/index.html"
+  }
+];
